cffd8b7cf653bf7e367ab8c8540cba753685a681
DmytroChuma <chuma.dmytro@student.uzhnu.edu.ua>
2023-06-16 18:03:33 +0300

    виправлено деякі баги

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const config = {
     PORT: process.env.PORT || '3001',
     SESSION_KEY: process.env.SESSION_KEY || 'development_test_key',
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_NAME: process.env.DB_NAME || '',
-    DB_PASSWORD: process.env.DB_PASSWORD || ''
+    DB_HOST: process.env.DB_HOST || 'sql8.freemysqlhosting.net',
+    DB_USER: process.env.DB_USER || 'sql8626676',
+    DB_NAME: process.env.DB_NAME || 'sql8626676',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'A9Xgw4bXE4'
 }
 
 module.exports = config;
