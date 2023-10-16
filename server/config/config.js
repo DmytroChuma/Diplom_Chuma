67d35738fae5b381f50bb4578c30cf8c3dba03cd
DmytroChuma <chuma.dmytro@student.uzhnu.edu.ua>
2023-10-16 21:53:18 +0300

    Swagger

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const config = {
     PORT: process.env.PORT || '3001',
     SESSION_KEY: process.env.SESSION_KEY || 'development_test_key',
-    DB_HOST: process.env.DB_HOST || 'sql8.freemysqlhosting.net',
-    DB_USER: process.env.DB_USER || 'sql8626676',
-    DB_NAME: process.env.DB_NAME || 'sql8626676',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'A9Xgw4bXE4'
+    DB_HOST: process.env.DB_HOST || 'sql11.freemysqlhosting.net',
+    DB_USER: process.env.DB_USER || 'sql11653781',
+    DB_NAME: process.env.DB_NAME || 'sql11653781',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'tuBFeRJxPh'
 }
 
 module.exports = config;
